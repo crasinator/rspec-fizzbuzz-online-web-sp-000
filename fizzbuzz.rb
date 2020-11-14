b0a7b7a94682a6b43010df92a5ee5b3fb56b90a7
Jordan Reed <jordanreed03@gmail.com>
2020-11-14 23:30:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(number)
+  if number / 3 && number / 5 == Integer
+    puts "fizzbuzz"
+  elsif number / 5 == Integer
+    puts "Buzz"
+  elsif number / 3 == Integer
+    puts "Fizz"
+  end
+end
