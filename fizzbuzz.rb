8fcfb1ee44805c87b353a2c125cb7afcecf27f1a
Jordan Reed <jordanreed03@gmail.com>
2020-11-14 23:37:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
-  if number / 3 && number / 5 == Integer
+  if number.to_i / 3 && number.to_i / 5 == Integer
     puts "FizzBuzz"
-  elsif number / 5 == Integer
+  elsif number.to_i / 5 == Integer
     puts "Buzz"
-  elsif number / 3 == Integer
+  elsif number.to_i / 3 == Integer
     puts "Fizz"
   else 
     puts "number is not divisible by 3 or 5"
+  end
 end
