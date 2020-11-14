005d8eb9a51337587eda7c3bc96df21f20897c38
Jordan Reed <jordanreed03@gmail.com>
2020-11-14 23:45:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
-  if number.to_i % 3 == 0 && number.to_i / 5 == 0 
+  if number.to_i % 3 == 0 && number.to_i % 5 == 0 
     puts "FizzBuzz"
   elsif number.to_i % 5 == 0
     puts "Buzz"
